ba5761eeea11a0c9f2a57fcae1b976089028a7cb
GSahiDarsini <gsahidarsini@gmail.com>
2016-10-19 19:24:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 var articles = {
     
-article-one:{
+`article-one:{
     title :' 햞ticle One !!! - GSD',
     heading : 'Article One',
     date : '19 th october, 2016',
@@ -15,14 +15,14 @@ article-one:{
     <p> enjoy life to the fullest because you never know what happens the next moment. take challenges which will haunt you but at the end will create a glorious aura around you </P>`
     
 },
-article-two:{ title :' 햞ticle Two !!! - GSD',
+`article-two:{ title :' 햞ticle Two !!! - GSD',
     heading : 'Article Two',
     date : '19 th october, 2016',
     content :` <p> Life is all about journey and not being a destination. we face many challenges but at the end of the day when we take them for the positively there in nothing on this earth that can hinder us from doing miracles. </p>
     <p> life is never and will never be rude to us , its all about the approach of the individual that decides where we land : in a paradise or in a hell. </p>
     <p> enjoy life to the fullest because you never know what happens the next moment. take challenges which will haunt you but at the end will create a glorious aura around you </P>`
     },
-article-three:{ title :' 햞ticle One !!! - GSD',
+`article-three:{ title :' 햞ticle One !!! - GSD',
     heading : 'Article One',
     date : '19 th october, 2016',
     content :` <p> Life is all about journey and not being a destination. we face many challenges but at the end of the day when we take them for the positively there in nothing on this earth that can hinder us from doing miracles. </p>
@@ -58,13 +58,16 @@ var htmlTemplate = {
     </body>
     </html>}
     return htmlTemplate ; }
+    app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+    
 app.get('/:articleName', function (req, res) {
   var articleName=req.params.articleName;
   res.send(path.join(createTemplate(articles(articleName))));
 });
-app.get('/article-one', function(req,res){
-    res.send(createTemplate(articleOne));
-})
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
